a29553dd542dd2c48162239b325722d4e716679f
s16701 <61627877+s16701@users.noreply.github.com>
2020-04-05 22:26:17 +0200

    Add files via upload

---
+++ Cw3/Cw3/Controllers/StudentsController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cw3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cw3.Controllers
+
+{
+    [ApiController]
+    [Route("api/students")]
+
+    public class StudentsController : ControllerBase
+
+    {
+        [HttpGet]
+        public string GetStudents(string orderBy)
+        {
+            return $"Kowalski, Malewski, Andrzejewski sortowanie={orderBy}";
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudentDetails(int id)
+        {
+           if (id == 1)
+            {
+                return Ok("Jan Kowalski");
+            }else if (id == 2)
+            {
+                return Ok("Andrzej Malewski");
+            }
+            return NotFound("Nie znaleziono studenta");
+        }
+
+        [HttpPost]
+
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 10000)}";
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateStudent(int id, Student student)
+        {
+            
+            return Ok(student);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult RemoveStudent(int id)
+        {
+            
+            return Ok(student);
+        }
+    }
+}
